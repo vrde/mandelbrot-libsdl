037d7e720d7632526e190a3f9e6eb564fd919578
vrde <agranzot@gmail.com>
2019-02-18 03:26:43 +0100

    Add code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mandelbrot-libsdl
+
+Compile and run with:
+```
+g++ mandelbrot.c -w -lSDL2 -o mandelbrot
+./mandelbrot
+```
+
+Make sure you have `libsdl2-dev` installed in your system.
---
+++ mandelbrot.c
@@ -0,0 +1,154 @@
+//Using SDL and standard IO
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <math.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const double SCREEN_RATIO = (double)SCREEN_HEIGHT / SCREEN_WIDTH;
+const int FPS = 30;
+const int CLOCK = 1000 / FPS;
+
+Uint32 pixels[SCREEN_WIDTH*SCREEN_HEIGHT*4];
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreen = NULL;
+SDL_Renderer* gRenderer = NULL;
+SDL_Texture* gTexture = NULL;
+double gScale = 2;
+double gOffsetX = 0;
+double gOffsetY = 0;
+double gLastScale = 0;
+
+double scaleX(int x) {
+  return (((double)x / SCREEN_WIDTH) - 0.5) * gScale + gOffsetX;
+}
+
+double scaleY(int y) {
+  return -(((double)y / SCREEN_HEIGHT * SCREEN_RATIO) - SCREEN_RATIO / 2) * gScale + gOffsetY;
+}
+
+void mandelbrot() {
+  for (int screenX = 0; screenX < SCREEN_WIDTH; screenX++) {
+    for (int screenY = 0; screenY < SCREEN_HEIGHT; screenY++) {
+      double x0 = scaleX(screenX);
+      double y0 = scaleY(screenY);
+      double x = 0;
+      double y = 0;
+      int iteration = 0;
+      while ((x*x + y*y <= 4) && iteration <= 0xff) {
+        double xtemp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtemp;
+        iteration++;
+      }
+      pixels[screenY * SCREEN_WIDTH + screenX] = iteration << 8;
+    }
+  }
+}
+
+void mandelbrot2() {
+  for (int screenX = 0; screenX < SCREEN_WIDTH; screenX++) {
+    for (int screenY = 0; screenY < SCREEN_HEIGHT; screenY++) {
+      double x0 = scaleX(screenX);
+      double y0 = scaleY(screenY);
+      double x = 0;
+      double y = 0;
+      int iteration = 0;
+      int maxIteration = 0xff;
+      while ((x*x + y*y <= (1 << 16)) && iteration < maxIteration) {
+        double xtemp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtemp;
+        iteration++;
+      }
+      if (iteration < maxIteration) {
+        double logZn = log(x*x + y*y) / 2;
+        double nu = log(logZn/log(2))/log(2);
+        iteration = iteration + 1 - nu;
+      }
+      pixels[screenY * SCREEN_WIDTH + screenX] = (int)floor(iteration) << 16;
+    }
+  }
+}
+
+int render() {
+  if(gLastScale != gScale) {
+    mandelbrot2();
+    SDL_UpdateTexture(gTexture, NULL, pixels, 640 * sizeof(Uint32));
+    SDL_RenderClear(gRenderer);
+    SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+    SDL_RenderPresent(gRenderer);
+    gLastScale = gScale;
+  }
+}
+
+void event(SDL_Event* e) {
+  switch(e->type) {
+    case SDL_MOUSEBUTTONUP:
+      gOffsetX = scaleX(e->motion.x);
+      gOffsetY = scaleY(e->motion.y);
+      if (e->button.button == SDL_BUTTON_LEFT) {
+        gScale *= 0.5;
+      } else if (e->button.button == SDL_BUTTON_RIGHT) {
+        gScale *= 1.5;
+      }
+      break;
+    case SDL_MOUSEBUTTONDOWN:
+      break;
+    case SDL_MOUSEMOTION:
+      char title[80];
+      sprintf(title, "offset x: %f, offset y: %f", scaleX(e->motion.x), scaleY(e->motion.y));
+      SDL_SetWindowTitle(gWindow, title);
+      break;
+  }
+}
+
+int loop() {
+  bool quit = false;
+  SDL_Event e;
+  unsigned int timer;
+  int delay;
+
+  while(!quit) {
+    timer = SDL_GetTicks();
+
+    while(SDL_PollEvent(&e) != 0) {
+      if(e.type == SDL_QUIT) {
+        quit = true;
+      } else {
+        event(&e);
+      }
+    }
+
+    render();
+    delay = CLOCK - (SDL_GetTicks() - timer);
+    SDL_Delay(delay > 0 ? delay : 0);
+  }
+}
+
+void boot() {
+  memset(pixels, 0, 640 * 480 * sizeof(Uint32));
+  if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+  } else {
+    gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+    gTexture = SDL_CreateTexture(gRenderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, 640, 480);
+
+    if(gWindow == NULL) {
+      printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    } else {
+      gScreen = SDL_GetWindowSurface(gWindow);
+      loop();
+    }
+  }
+  SDL_DestroyRenderer(gRenderer);
+  SDL_DestroyWindow(gWindow);
+  SDL_Quit();
+}
+
+int main( int argc, char* args[] ) {
+  boot();
+  return 0;
+}
